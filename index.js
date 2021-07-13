196173c397b3176c5c47bf02b2b2b7252fbf3363
Corey <corey.amano@gmail.com>
2021-07-12 19:12:39 -0700

    add v-if

--- index.js
+++ index.js
@@ -5,11 +5,19 @@ var app = new Vue({
     return {
       message: "Hello from JavaScript!",
       message2: "This is the input box text",
+      seen: true
     };
   },
   methods: {
     reverseMessage: function () {
       this.message = this.message.split('').reverse().join('');
+    },
+    showMore: function() {
+      if (this.seen === true) {
+        this.seen = false;
+      } else {
+        this.seen = true;
+      }
     }
   }
 });
