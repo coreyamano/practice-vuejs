157fa5c4037de3288c829cf57f6ad127e2e388f3
Corey <corey.amano@gmail.com>
2021-07-13 17:18:57 -0700

    add axios get request

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
-/* global Vue */
+/* global Vue, axios */
 var app = new Vue({
   el: "#app",
   data: function() {
     return {
       message: "Hello from JavaScript!",
       message2: "This is the input box text",
-      seen: true
+      seen: true,
+      animals: ["dog", "cat", "fish"],
+      newAnimal: "",
+      todos: []
     };
   },
   methods: {
@@ -18,6 +21,14 @@ var app = new Vue({
       } else {
         this.seen = true;
       }
+    },
+    loadTodos: function() { 
+      axios
+        .get("https://jsonplaceholder.typicode.com/todos")
+        .then(response => {
+          console.log(response.data);
+          this.todos = response.data;
+        });
     }
   }
 });
